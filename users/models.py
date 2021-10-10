3e71ba18691005b1bf7edc39e25f1e42653ccc08
Gilson Gindre <gindrejunior@hotmail.com>
2021-10-10 10:31:12 -0300

    created user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField('email', max_length=30, unique=True)
+    first_name = models.CharField(_('first name'), max_length=30)
+    last_name = models.CharField(_('last name'), max_length=30)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('first_name', 'last_name')
