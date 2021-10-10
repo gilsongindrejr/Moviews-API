9e2363640b46f1cb6416be388ceea82ca98a2ace
Gilson <gindrejunior@hotmail.com>
2021-10-10 13:20:00 -0300

    registered movie model on admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Movie
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('name', 'director', 'release_date')
+    list_filter = ('director', 'release_date')
+    search_fields = ('name', 'director', 'release_date')
+    ordering = ('name',)
