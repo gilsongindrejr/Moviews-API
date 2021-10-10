40bb95a2bf3995e8ebcfb41eeae781579cca0582
Gilson <gindrejunior@hotmail.com>
2021-10-10 13:01:44 -0300

    created movie model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Movie(models.Model):
+    name = models.CharField(_('name'), max_length=100)
+    release_date = models.DateField(_('release date'))
+    director = models.CharField(_('director'), max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Movie'
+        verbose_name_plural = 'Movies'
