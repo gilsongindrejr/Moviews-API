60cee76330ba170962130ead52a4dfa0cc6533a8
Gilson <gindrejunior@hotmail.com>
2021-10-10 13:09:24 -0300

    created rating model

--- movies/models.py
+++ movies/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
 
 class Movie(models.Model):
@@ -13,3 +14,14 @@ class Movie(models.Model):
     class Meta:
         verbose_name = 'Movie'
         verbose_name_plural = 'Movies'
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(get_user_model(), verbose_name=_('user'), on_delete=models.CASCADE, related_name='ratings')
+    movie = models.ForeignKey(Movie, verbose_name=_('movie'), on_delete=models.CASCADE, related_name='ratings')
+    title = models.CharField(_('title'), max_length=100)
+    rating = models.DecimalField(max_digits=2, decimal_places=1)
+    comment = models.TextField(_('comment'), max_length=255)
+
+    def __str__(self):
+        return f'{self.user} rated {self.rating} on movie {self.movie}'
