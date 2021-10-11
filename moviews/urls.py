eb1ba4566038806f7a6786bc33403e672d1580f1
Gilson <gindrejunior@hotmail.com>
2021-10-11 12:34:25 -0300

    created API paths

--- moviews/urls.py
+++ moviews/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from movies.urls import router
 
 urlpatterns = [
+    path('api/v1/', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('auth/', include('rest_framework.urls')),
 ]
