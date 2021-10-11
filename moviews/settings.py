61b78f4eb4f5158ed8c101fe8387a7ae2520f801
Gilson <gindrejunior@hotmail.com>
2021-10-11 12:32:13 -0300

    rest framework settings

--- moviews/settings.py
+++ moviews/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
 
     'users',
     'movies',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,22 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/minute',
+        'user': '10/minute',
+    }
+}
