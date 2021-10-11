f258b0474b9995df42133c66c36e6276a93a3c38
Gilson <gindrejunior@hotmail.com>
2021-10-11 12:31:33 -0300

    created modelviewsets

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
-# Create your views here.
+from .models import Movie, Rating
+from .serializers import MovieSerializer, RatingSerializer
+
+
+class MoviesViewSet(viewsets.ModelViewSet):
+    serializer_class = MovieSerializer
+    queryset = Movie.objects.all()
+
+    @action(detail=True, methods=['get'])
+    def ratings(self, request, pk=None):
+        movie = self.get_object()
+        serializer = RatingSerializer(movie.ratings.all(), many=True)
+        return Response(serializer.data)
+
+
+class RatingsViewSet(viewsets.ModelViewSet):
+    serializer_class = RatingSerializer
+    queryset = Rating.objects.all()
